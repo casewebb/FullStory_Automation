5d066bc1211b58cc2f6a90c1353e9f5aa86fe8bd
Case <case.webb@gmail.com>
2021-09-23 22:55:39 -0400

    test package name broke..

--- tests/test_suite/TestRunner.py
+++ tests/test_suite/TestRunner.py
@@ -1,12 +1,8 @@
 from unittest import TestLoader, TestSuite, TextTestRunner
-
-from test.scripts.test_FullStory import FullStoryTest
-
-import testtools as testtools
+from tests.scripts.test_FullStory import FullStoryTest
 
 if __name__ == "__main__":
     test_loader = TestLoader()
-    # Test Suite is used since there are multiple test cases
     test_suite = TestSuite((
         test_loader.loadTestsFromTestCase(FullStoryTest),
     ))
